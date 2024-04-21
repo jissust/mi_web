ca04790de0aa318e40aabffbaf5e0f6b571ab2c4
jesustissera <jesustissera@gmail.com>
2024-04-21 02:10:42 -0300

    Al refrescarse permanece activa la selección en el menú

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,21 +1,50 @@
-import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { RouterLinkActive, Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterLinkActive, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+
+export class NavbarComponent implements OnInit{
   stateScroll  = false;
-  @HostListener('document:scroll')
+  public option!: string;
   
+  constructor(private route: ActivatedRoute){
+    
+  }
+
+  ngOnInit(): void {
+    this.route.fragment
+    .pipe()
+    .subscribe({
+      next: (element) => {
+        this.option = 'home';
+        if(element != null){
+          this.option = element;
+        }
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    })
+  }
+
+  @HostListener('document:scroll')
+
   scrollFunction(){
     if(document.documentElement.scrollTop > 10){
       this.stateScroll = true
     }else{
       this.stateScroll = false
     }
+  }
+
+  selectOption(option: string){
+    this.option = option;
   }
 }
